cb043ea37f041a10b26c714b0b933207ed6df3de
Alex Yang <alyx.yang@gmail.com>
2014-04-24 02:12:10 -0700

    refactored alot of code for speed improvements

--- Gemfile
+++ Gemfile
@@ -18,7 +18,6 @@ gem 'fancybox2-rails'
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,21 +3,19 @@ class HomeController < ApplicationController
   def index
         result = []
         next_id = nil
-        while result.length < 200
-            data = Instagram.tag_recent_media("geisel", max_id: next_id, count: 50)
+        pages = 0;
+        while pages < 5
+            data = Instagram.tag_recent_media("geisel", max_id: next_id)
             next_id = data.pagination.next_max_id
             result.concat(data)
+            pages += 1
         end
-        #result.each do |instagram_pic|
-        #    data = JSON.parse(instagram_pic.to_json)
-        #end
 
-        @results = result.paginate(:page => params[:page], :per_page => 40)
+        @results = result.paginate(:page => params[:page], :per_page => 30)
+
           if request.xhr?
             render '_photos'
           end
-        #new_max_id = @instagram.pagination.next_max_id
-        #@nextpage = Instagram.tag_recent_media("geisel", :max_id => new_max_id ) unless new_max_id.nil?
   end
 
   def about
